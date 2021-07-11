32359de8e1f3e1a51fa194eb1ec53957e8c06e7b
Noe <asdz453@gmail.com>
2021-07-11 20:36:55 +0900

    홈 화면, 로그인 화면 간단하게 만들고 로그인 값 받아보기

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,6 +15,24 @@ public class MemberController {
     @Autowired // Autowired를 쓰면 스프링 컨테이너에서 멤버 서비스를 가져옴
     public MemberController(MemberService memberService) { // 생성자
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member = " + member.getName());
+
+
+        memberService.join(member);
+        return "redirect:/";
     }
 }
 
